d5c0999970a0df42385a48690a03b0a34e11a80f
Naman Dixit <code@namandixit.net>
2020-06-02 14:02:13 +0530

    Changed the test file to use Ryu float printing print

--- test.c
+++ test.c
@@ -3,14 +3,15 @@
  * Notice: © Copyright 2019 Naman Dixit
  */
 #define NLIB_TESTS
-#define NLIB_PRINT_BAD_FLOAT
+//#define NLIB_PRINT_BAD_FLOAT
 #include "nlib.h"
 
 Sint main (Sint argc, Char *argv[])
 {
     unused_variable(argc);
     unused_variable(argv);
-
+//    say("%e", -3.0);
+//    say("\n");
 # if !defined(NLIB_NO_LIBC)
 # endif
     sbufUnitTest(); // Uses sbufPrint, so tests might clash with printUnitTest
